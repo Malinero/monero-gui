aa1c8c9a7433e2506ae1eaa8bf02814a7c0666b7
malinero <malinero@protonmail.com>
2025-02-21 01:03:43 +0100

    tmp win

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -75,7 +75,7 @@ if(APPLE OR (WIN32 AND NOT STATIC))
         )
 
         add_custom_command(TARGET monero-wallet-gui POST_BUILD
-                           COMMAND "${CMAKE_COMMAND}" -E env PATH="${_qml_bin_dir}:${_qt_bin_dir}:$ENV{PATH}" "${WINDEPLOYQT_EXECUTABLE}" "$<TARGET_FILE:monero-wallet-gui>" -no-translations -qmldir="${CMAKE_SOURCE_DIR}" -verbose=3
+                           COMMAND "${CMAKE_COMMAND}" -E env PATH="${_qml_bin_dir};${_qt_bin_dir};$ENV{PATH}" "${WINDEPLOYQT_EXECUTABLE}" "$<TARGET_FILE:monero-wallet-gui>" -no-translations -qmldir="${CMAKE_SOURCE_DIR}" -verbose=3
                            COMMENT "Running windeployqt..."
         )
         set(WIN_DEPLOY_DLLS
