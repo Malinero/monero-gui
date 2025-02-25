39fc29a91f034d116036bd4884ec1f77e55697ff
malinero <malinero@protonmail.com>
2025-02-25 12:14:15 +0100

    fix up

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -49,11 +49,12 @@ if(APPLE OR (WIN32 AND NOT STATIC))
 
     elseif(WIN32)
         find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
-        get_target_property(_qmlimportscanner_executable Qt5::qmlimportscanner IMPORTED_LOCATION)
-        get_filename_component(_qml_bin_dir "${_qmlimportscanner_executable}" DIRECTORY)
+
+        #get_target_property(_qmlimportscanner_executable Qt5::qmlimportscanner IMPORTED_LOCATION)
+        #get_filename_component(_qml_bin_dir "${_qmlimportscanner_executable}" DIRECTORY)
 
         add_custom_command(TARGET monero-wallet-gui POST_BUILD
-                           COMMAND "${CMAKE_COMMAND}" -E env PATH="${_qml_bin_dir}\;${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}" "$<TARGET_FILE:monero-wallet-gui>" -no-translations -qmldir="${CMAKE_SOURCE_DIR}"
+                           COMMAND "${CMAKE_COMMAND}" -E env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}" "$<TARGET_FILE:monero-wallet-gui>" -no-translations -qmldir="${CMAKE_SOURCE_DIR}" -verbose=3
                            COMMENT "Running windeployqt..."
         )
         set(WIN_DEPLOY_DLLS
